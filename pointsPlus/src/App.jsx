6ce6974c724c9c8740037cd02ccaf8b3ad2ee7da
Alvaro <alvaro.a.cruz.j@gmail.com>
2023-10-26 11:21:25 -0600

    routing functio

--- pointsPlus/src/App.jsx
+++ pointsPlus/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import ShoppingScreen from './ShoppingScreen.jsx';
+import About from './About';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Router>
+            <div className="app-container">
+                <div className="navbar">
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">+</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">+</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<ShoppingScreen />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
---
+++ pointsPlus/src/ShoppingScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logini from './assets/logini.svg'; // Asegúrate de que la ruta coincida con la ubicación de tu archivo SVG.
+import './App.css';
+
+const ShoppingScreen = () => {
+    return <div>shooping Page</div>;
+};
+
+export default ShoppingScreen;
